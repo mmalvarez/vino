7d2c9ed84fbe660f791ed70471a464da3ab8ce2d
Mario Alvarez <mario.mcgilvray.alvarez@gmail.com>
2017-05-24 17:38:13 -0700

    more invertible syntax

--- src/InvertibleSyntax.v
+++ src/InvertibleSyntax.v
@@ -1,13 +1,12 @@
 Require Import Coq.Strings.String.
 
-Section Isos.
+Inductive Pwrap : Prop -> Prop :=
+| Pw : forall (P : Prop), P -> Pwrap P.
 
-  Inductive Pwrap : Prop -> Prop :=
-  | Pw : forall (P : Prop), P -> Pwrap P.
-
-  Notation " !PW T " := (Pw T _) (at level 0).
+Notation " !PW T " := (Pw T _) (at level 0).
   
-  Structure Piso T1 T2 := MkPiso
+Record Piso T1 T2 :=
+  MkPiso
     { l2r : (T1 -> option T2);
       r2l : (T2 -> option T1);
       _ : Pwrap (forall a b,
@@ -16,7 +15,7 @@ Section Isos.
 
   (* Q: should these be canonical instances? *)
   (* Q: should we hide the proof? *)
-  Canonical Structure inil (T : Type) : Piso unit (list T).
+  Definition inil (T : Type) : Piso unit (list T).
   Proof.
     refine (MkPiso _ _
                    (fun _ => Some nil)
@@ -29,7 +28,7 @@ Section Isos.
       simpl; first [reflexivity | inversion 1].
   Defined.
 
-  Canonical Structure icons (T : Type) : Piso (T * list T) (list T).
+  Definition icons (T : Type) : Piso (T * list T) (list T).
   Proof.
     refine (MkPiso _ _
                    (fun p =>
@@ -67,21 +66,115 @@ Section Isos.
   Definition unapply {T1 T2 : Type} : (Piso T1 T2) -> (T2) -> option T1 :=
     comp apply inverse.
 
-  Structure Category (T : Type -> Type -> Type) : Type :=
-    MkCat { cid {A : Type} : T A A
-            ; ccomp {A : Type} {B : Type} {C : Type} : T B C -> T A B -> T A C
-            ; _ : Pwrap
-                    (forall (A B C D : Type)
-                       (f : T C D) (g : T B C) (h : T A B),
-                        ccomp (ccomp f g) h = ccomp f (ccomp g h))
-            ; _ : Pwrap
-                    (forall (A B : Type) (f : T A B),
-                        ccomp f cid = f)
-            ; _ : Pwrap
-                    (forall (A B : Type) (f : T A B),
-                        ccomp cid f = f)
+(* TODO can we use setoids instead *)
+Axiom funex : forall (A B : Type) (f g : A -> B),
+    (forall x, f x = g x) -> f = g.
 
+(* Example of Mahboubi-style Canonicals-as-Typeclasses *)
+Module Functor.
+  Record class (T : Type -> Type) : Type :=
+    Class { fmap' : forall (A A' : Type), (A -> A') -> T A -> T A'
+            ; _ : Pwrap (forall (A : Type), fmap' A A id = id)
+            ; _ : Pwrap (forall (A B C : Type) (g : B -> C) (f : A -> B),
+                fmap' A C (comp g f) = comp (fmap' _ _ g) (fmap' _ _ f))}.
+
+  Structure type := Pack {obj : Type -> Type; class_of : class obj }.
+
+  Definition fmap (E : type) :
+    forall {A A' : Type}, (A -> A') -> (obj E) A -> (obj E) A' :=
+    fun A A' => fmap' (obj E)
+                   (class_of E)
+                   A A'.
+
+  Arguments fmap {_ _ _} _ _.
+  Arguments Class {_} _ _ _.
+End Functor.
+
+Definition list_FunctorClass : Functor.class list.
+Proof.
+  refine (Functor.Class List.map _ _).
+  - split. intros. apply funex.
+    induction x. reflexivity.
+    simpl. rewrite IHx. reflexivity.
+  - split. intros. apply funex.
+    induction x; simpl. reflexivity.
+    rewrite IHx. reflexivity.
+Defined.
+
+Canonical Structure list_FunctorTy : Functor.type := Functor.Pack list list_FunctorClass.
+Eval compute in (Functor.fmap (fun x => x + 1) nil).
+
+Definition option_FunctorClass : Functor.class option.
+Proof. refine (Functor.Class
+                 (fun _ _ f o => match o with
+                              | Some x => Some (f x)
+                              | _ => None end) _ _).
+       - split. intros. apply funex.
+         destruct x; reflexivity.
+       - split; intros. apply funex. destruct x; reflexivity.
+Defined.
+
+Canonical Structure option_FunctorTy : Functor.type :=
+  Functor.Pack option option_FunctorClass.
+
+Eval compute in (Functor.fmap (fun x => x + 1) (Some 2)).
+
+(* Another, more example, more relevant to our case - Category *)
+Module Category.
+  Record class (T : Type -> Type -> Type) : Type :=
+    Class { cid' : forall (A : Type), T A A
+            ; ccomp' : forall (A B C : Type), T B C -> T A B -> T A C
+            ; _ : Pwrap (forall (A B C D : Type)
+                           (f : T C D) (g : T B C) (h : T A B),
+                            ccomp' _ _ _ f (ccomp' _ _ _ g h) = ccomp' _ _ _ (ccomp' _ _ _ f g) h
+                        )
+            ; _ : Pwrap (forall (A B : Type)
+                           (f : T A B), ccomp' _ _ _ f (cid' A) = f
+                        )
+            ; _ : Pwrap (forall (A B : Type)
+                           (f : T A B), ccomp' _ _ _ (cid' B) f = f
+                    )
           }.
+
+  Structure type := Pack {obj : Type -> Type -> Type; class_of : class obj}.
+
+  Definition cid (E : type) :
+    forall {A : Type}, (obj E) A A := fun A => cid' (obj E) (class_of E) A.
+  Definition ccomp (E : type) :
+    forall {A B C : Type}, (obj E) B C -> (obj E) A B -> (obj E) A C :=
+    fun A B C => ccomp' (obj E) (class_of E) A B C.
+
+  Arguments cid {_} _.
+  Arguments ccomp {_} _ _ _ _ _.
+End Category.
+
+(* TODO support this for arbitrary monads *)
+Definition thread {A B C : Type}
+           (f : A -> option B) (g : B -> option C) (a : A) : option C :=
+  match f a with
+  | Some b => match g b with
+             | Some c => Some c
+             | _ => None
+             end
+  | _ => None
+  end.
+
+Check Category.Class.
+  
+Definition Piso_CategoryClass : Category.class Piso.
+Proof.
+  refine (Category.Class ()
+            (fun _ _ 
+
+
+  Check fmap.
+  Arguments fmap {_ _ _ _} _ _.
+
+  Check (fmap (fun x => x + 1) nil).
+
+  Eval compute in (fmap (fun x => x + 1) nil).
+
+  Check fmap.
 
   Check MkCat.
   Check MkPiso.
